f59936717a68854a3a3835c35415f1aeb07e333d
pouloune <dasseshella@gmail.com>
2024-09-30 15:32:15 -0400

    CRUD with postman

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ let users = [
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
   // Copy the code here
-  res.send(users);
+  res.send(JSON.stringify({users}, null, 4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
@@ -40,6 +40,17 @@ router.get("/:email",(req,res)=>{
    res.send(filtered_users);
   res.send(filtered_users);//This line is to be replaced with actual return value
 });
+
+router.get("/lastName/:lastName", (req, res) => {
+    // Extract the lastName parameter from the request URL
+    const lastName = req.params.lastName;
+    // Filter the users array to find users whose lastName matches the extracted lastName parameter
+    let filtered_lastname = users.filter((user) => user.lastName === lastName);
+    // Send the filtered_lastname array as the response to the client
+    res.send(filtered_lastname);
+});
+
+
 
 
 // POST request: Create a new user
@@ -111,5 +122,22 @@ router.delete("/:email", (req, res) => {
     res.send(`User with the email ${email} deleted.`);
 });
 
+// Function to convert a date string in the format "dd-mm-yyyy" to a Date object
+function getDateFromString(strDate) {
+    let [dd, mm, yyyy] = strDate.split('-');
+    return new Date(yyyy + "/" + mm + "/" + dd);
+}
+
+// Define a route handler for GET requests to the "/sort" endpoint
+router.get("/sort", (req, res) => {
+    // Sort the users array by DOB in ascending order
+    let sorted_users = users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+        return d1 - d2;
+    });
+    // Send the sorted_users array as the response to the client
+    res.send(sorted_users);
+});
 
 module.exports=router;
